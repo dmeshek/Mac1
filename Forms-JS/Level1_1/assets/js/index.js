1d8fd391f13e01068a88873955c1dec0d67814f2
macbook <macbook@MacBook-Air-von-macbook.local>
2024-04-19 15:32:53 +0200

    Test

--- Forms-JS/Level1_1/assets/js/index.js
+++ Forms-JS/Level1_1/assets/js/index.js
@@ -1,3 +1,5 @@
+
+//Function Level 1_1
 function VerdoppelMich(){
     let inputNumber = document.querySelector("#alter").value;
     let dieVerdoppelteZahl = inputNumber*2;
@@ -5,3 +7,10 @@ function VerdoppelMich(){
     document.getElementById("ergebnis").innerHTML = dieVerdoppelteZahl;
 }
 
+//Function Level 1_2
+function berechneAlter(){
+    let inputJahr = document.querySelector("#jahr").value;
+    let AlterInJahr = 2024 - inputJahr;
+    console.log(AlterInJahr);
+    document.getElementById("ergebnisZwei").innerHTML = `du bist${AlterInJahr} Jahre Alt`;
+}
